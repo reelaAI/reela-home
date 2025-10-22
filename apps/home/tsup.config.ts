d64be29c7ceebbc2c3b05992e20032881d214c8e
ocherry341 <91065936+ocherry341@users.noreply.github.com>
2025-10-22 12:02:28 +0800

    fix: build

--- apps/home/tsup.config.ts
+++ apps/home/tsup.config.ts
@@ -1,18 +1,14 @@
 import { defineConfig } from "tsup";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig((options) => {
   return {
     ...options,
-    outDir: "dist",
+    outDir: "./",
     entry: ["./src/index.ts"],
     splitting: true,
     format: "esm",
     sourcemap: false,
-    clean: true,
+    clean: false,
     loader: {
       ".css": "text",
     },
